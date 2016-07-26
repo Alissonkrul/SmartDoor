638972720665e65e053769b718a076efb490d50f
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2016-07-26 18:00:28 -0300

    feat(RFID): now is possible add and remove rfid in memory

--- Sketchs/sketch_main/sketch_main.ino
+++ Sketchs/sketch_main/sketch_main.ino
@@ -1,5 +1,4 @@
 // Load Libraries
-
 #include <pt.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -8,7 +7,7 @@
 #include <NuidUtils.h>
 #include <LiquidCrystal_I2C.h>
 
-// Define Const
+// Define Consts
 #define      BT_TX 2 
 #define      BT_RX 3 
 #define    WIFI_TX 4 
@@ -25,7 +24,9 @@
 #define      RELE A3 
 #define   LCD_SDA A4 
 #define   LCD_SCL A5 
- 
+
+#define  openTime 2
+
 enum State {
   locked,
   programMode,
@@ -39,80 +40,90 @@ enum LedColor {
   none
 };
 
+// Variables
 State state = locked;
-int openTime = 2;
 signed long lastTime;
-
-
-//const byte *masterKey[] = {0x7C 0x68 0xC0 0x04};
 byte  masterKey[] = {0x7C,0x68,0xC0,0x04};
-
-// Initialize the RFID shield
-MFRC522 rfid(RFID_SDA, RFID_RST);
 MFRC522::MIFARE_Key key; 
-
 NuidUtils nuidUtils;
 
-// Inicializa o display no endereco 0x27
+// Initializers
+MFRC522 rfid(RFID_SDA, RFID_RST);
 LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3, POSITIVE);
 
-void printHex(byte *buffer, int bufferSize) {
+// Prototype Functions
+void printHex(byte *buffer, int bufferSize);
+void setLED(LedColor color);
+void setState(State newState);
+void loadState();
+void stateLocked();
+void stateOpen();
+void stateProgramMode();
+
+void setup() { 
+  Serial.begin(9600);
+  lcd.begin (16,2);
+  SPI.begin();
+
+  rfid.PCD_Init();
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  pinMode(BTN, INPUT); 
+  pinMode(RELE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT); 
+  pinMode(LED_RED, OUTPUT);
+
+  setState(locked);
+
+  nuidUtils.reset();
+}
+
+void loop() {  
+  loadState();
+
+  /*if (digitalRead(BTN) == HIGH)
+  {
+    setState(opened);
+    return;
+    }*/
+  }
+
+  void printHex(byte *buffer, int bufferSize) 
+  {
     for (byte i = 0; i < bufferSize; i++) {
       Serial.print(buffer[i] < 0x10 ? " 0" : " ");
       Serial.print(buffer[i]);
     }
     Serial.print("\n");
-}
-
-void setup() { 
-  //Serial setup
-  Serial.begin(9600);
-
-  // Lcd setup
-  lcd.begin (16,2);
-  
-  //RFID setup
-  SPI.begin(); // Init SPI bus
-  rfid.PCD_Init(); // Init MFRC522 
-    for (byte i = 0; i < 6; i++) {
-    key.keyByte[i] = 0xFF;
   }
 
-  pinMode(RELE, OUTPUT);
-  pinMode(BTN, INPUT); 
-
-  // LED
-  pinMode(LED_GREEN, OUTPUT); 
-  pinMode(LED_RED, OUTPUT);
-
-  setState(locked);
-}
-
-void setLED(LedColor color)
-{
-  switch (color) {
+  void setLED(LedColor color)
+  {
+    switch (color) {
       case red:
-        digitalWrite(LED_RED, HIGH);
-        digitalWrite(LED_GREEN, LOW);
-        break;
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      break;
       case green:
-        digitalWrite(LED_RED, LOW);
-        digitalWrite(LED_GREEN, HIGH);
-        break;
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, HIGH);
+      break;
       case yellow:
-        digitalWrite(LED_RED, HIGH);
-        digitalWrite(LED_GREEN, HIGH);
-        break;
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, HIGH);
+      break;
       default:
-        digitalWrite(LED_RED, LOW);
-        digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, LOW);
+    }
   }
-}
 
-void setState(State newState)
-{
-  switch (newState) {
-    case locked:
+  void setState(State newState)
+  {
+    switch (newState) {
+      case locked:
       Serial.println("Door is LOCKED");
       
       digitalWrite(RELE, HIGH);
@@ -131,13 +142,13 @@ void setState(State newState)
       digitalWrite(BUZZER,HIGH);
       delay(400);
       digitalWrite(BUZZER,LOW);
-     break;
+      break;
 
-    case opened:
+      case opened:
       Serial.println("Door is OPEN");
 
       digitalWrite(RELE,LOW);
-    
+
       lcd.clear();
       lcd.setCursor(5, 0);
       lcd.print("ACESSO");
@@ -147,9 +158,9 @@ void setState(State newState)
 
       lastTime = millis();
       break;
-     default:
+      default:
       Serial.println("Programming MODE");
-       
+
       lcd.clear();
       lcd.setCursor(6, 0);
       lcd.print("MODO");
@@ -158,84 +169,92 @@ void setState(State newState)
       setLED(yellow);
 
       lastTime = millis();
-  }
-  state = newState;
-}
-
-void stateLocked()
-{
-  
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return;
-
-  if ( ! rfid.PICC_ReadCardSerial())
-    return;
-    
-  Serial.print("\nNUID: ");
-  printHex(rfid.uid.uidByte,4);;
-
-  if(nuidUtils.compare(masterKey,rfid.uid.uidByte))
-  {
-    Serial.println("Master key found!");
-    setState(programMode);
-    return;
+    }
+    state = newState;
   }
 
-  Serial.println("Searching rfid in memory...");
-  if(nuidUtils.search(rfid.uid.uidByte))
+  void loadState()
   {
-    Serial.println("RFID key found in memory!");
-    setState(opened);
-    return;
+    switch (state) {
+      case opened:
+      stateOpen();
+      break;
+      case programMode:
+      stateProgramMode();
+      break;
+      default:
+      stateLocked();
+
+    }
   }
 
-  Serial.println("RFID not permitted!");
-  setState(locked);
-}
-
-void stateOpen()
-{
-  digitalWrite(BUZZER,HIGH);
-  if(millis() >=  lastTime + (openTime * 1000) )
+  void stateLocked()
   {
+    if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+
+    if ( ! rfid.PICC_ReadCardSerial())
+    return;
+
+    Serial.print("\nNUID: ");
+    printHex(rfid.uid.uidByte,4);;
+
+    if(nuidUtils.compare(masterKey,rfid.uid.uidByte))
+    {
+      Serial.println("Master key found!");
+      setState(programMode);
+      return;
+    }
+
+    Serial.println("Searching rfid in memory...");
+    if(nuidUtils.search(rfid.uid.uidByte))
+    {
+      Serial.println("RFID key found in memory!");
+      setState(opened);
+      return;
+    }
+
+    Serial.println("RFID not permitted!");
     setState(locked);
-    return;
   }
 
-}
-
-void stateProgramMode()
-{
-  if ( ! rfid.PICC_IsNewCardPresent() && ! rfid.PICC_ReadCardSerial() )
+  void stateOpen()
   {
-    if(millis() > lastTime + 5000)
+    digitalWrite(BUZZER,HIGH);
+    if(millis() >=  lastTime + (openTime * 1000) )
     {
       setState(locked);
       return;
     }
   }
-  else if(nuidUtils.compare(masterKey,rfid.uid.uidByte))
+
+  void stateProgramMode()
   {
-    setState(opened);
-    return;
+    if ( ! rfid.PICC_IsNewCardPresent())
+    {
+      if(millis() > lastTime + 5000 )
+        setState(locked);
+      return;
+    }
+
+    if ( ! rfid.PICC_ReadCardSerial())
+      return;
+
+    if(nuidUtils.compare(masterKey,rfid.uid.uidByte))
+    {
+      if (millis() > lastTime + 500 )
+        setState(opened);
+      return;
+    }
+
+    if( !nuidUtils.search(rfid.uid.uidByte))
+    {
+      nuidUtils.add(rfid.uid.uidByte);
+      setState(opened);
+      return;
+    }
+
+    nuidUtils.removeByUid(rfid.uid.uidByte);
+    setState(locked);
+    return;  
   }
-}
-
-void loadState()
-{
-  switch (state) {
-      case opened:
-        stateOpen();
-        break;
-      case programMode:
-        stateProgramMode();
-        break;
-      default:
-        stateLocked();
-
-  }
-}
-
-void loop() {  
-  loadState();
-}
